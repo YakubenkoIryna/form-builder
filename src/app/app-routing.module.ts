1a6638ce41a79756b5b9b1bc4380f191f90a42cd
Ирина <Yakubenko1812@gmail.com>
2021-08-04 16:12:13 +0300

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./components/main-layout/main-layout.component";
+import {HomePageComponent} from "./components/home-page/home-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '' ,component: HomePageComponent}
+    ]
+  },
+  {
+    path: 'admin', loadChildren: './admin.module#AdminModule'
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MainLayoutComponent} from "./components/main-layout/main-layout.component";
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {AdminModule} from "./admin.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    BrowserAnimationsModule,
+
+
   ],
   providers: [],
+  exports: [
+    HeaderComponent,
+    MainLayoutComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
